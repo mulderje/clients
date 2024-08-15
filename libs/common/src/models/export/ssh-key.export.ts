eaeaa5d65cd42d955bd255f01e52fcfd4d5dedea
Bernd Schoolmann <mail@quexten.com>
2024-08-15 19:44:47 +0200

    Add ssh key import export for bitwarden json

---
+++ libs/common/src/models/export/ssh-key.export.ts
@@ -0,0 +1,49 @@
+import { SSHKeyView } from "@bitwarden/common/vault/models/view/ssh-key.view";
+
+import { EncString } from "../../platform/models/domain/enc-string";
+import { SSHKey as SSHKeyDomain } from "../../vault/models/domain/ssh-key";
+
+import { safeGetString } from "./utils";
+
+export class SSHKeyExport {
+  static template(): SSHKeyExport {
+    const req = new SSHKeyExport();
+    req.privateKey = "";
+    req.publicKey = "";
+    req.keyFingerprint = "";
+    req.keyAlgorithm = "";
+    return req;
+  }
+
+  static toView(req: SSHKeyExport, view = new SSHKeyView()) {
+    view.privateKey = req.privateKey;
+    view.publicKey = req.publicKey;
+    view.keyFingerprint = req.keyFingerprint;
+    view.keyAlgorithm = req.keyAlgorithm;
+    return view;
+  }
+
+  static toDomain(req: SSHKeyExport, domain = new SSHKeyDomain()) {
+    domain.privateKey = req.privateKey != null ? new EncString(req.privateKey) : null;
+    domain.publicKey = req.publicKey != null ? new EncString(req.publicKey) : null;
+    domain.keyAlgorithm = req.keyAlgorithm != null ? new EncString(req.keyAlgorithm) : null;
+    domain.keyFingerprint = req.keyFingerprint != null ? new EncString(req.keyFingerprint) : null;
+    return domain;
+  }
+
+  privateKey: string;
+  publicKey: string;
+  keyFingerprint: string;
+  keyAlgorithm: string;
+
+  constructor(o?: SSHKeyView | SSHKeyDomain) {
+    if (o == null) {
+      return;
+    }
+
+    this.privateKey = safeGetString(o.privateKey);
+    this.publicKey = safeGetString(o.publicKey);
+    this.keyFingerprint = safeGetString(o.keyFingerprint);
+    this.keyAlgorithm = safeGetString(o.keyAlgorithm);
+  }
+}
