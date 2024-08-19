e10e618644b270e8056bc99d944e0337e7538a94
Cesar Gonzalez <cgonzalez@bitwarden.com>
2024-08-19 10:05:58 -0500

    [PM-10669] Incorporating styles for select element

--- apps/browser/src/autofill/content/bootstrap-autofill-overlay-notifications.ts
+++ apps/browser/src/autofill/content/bootstrap-autofill-overlay-notifications.ts
@@ -1,4 +1,3 @@
-import { AutofillInlineMenuContentService } from "../overlay/inline-menu/content/autofill-inline-menu-content.service";
 import { OverlayNotificationsContentService } from "../overlay/notifications/content/overlay-notifications-content.service";
 import { AutofillOverlayContentService } from "../services/autofill-overlay-content.service";
 import { DomQueryService } from "../services/dom-query.service";
@@ -16,16 +15,14 @@ import AutofillInit from "./autofill-init";
       inlineMenuFieldQualificationService,
     );
 
-    let inlineMenuElements: AutofillInlineMenuContentService;
     let overlayNotificationsContentService: OverlayNotificationsContentService;
     if (globalThis.self === globalThis.top) {
-      inlineMenuElements = new AutofillInlineMenuContentService();
       overlayNotificationsContentService = new OverlayNotificationsContentService();
     }
 
     windowContext.bitwardenAutofillInit = new AutofillInit(
       autofillOverlayContentService,
-      inlineMenuElements,
+      null,
       overlayNotificationsContentService,
     );
     setupAutofillInitDisconnectAction(windowContext);
