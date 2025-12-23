3fbb4aced929f3a7904c54091b26a82765ba82e6
Bernd Schoolmann <mail@quexten.com>
2025-12-23 16:27:25 +0100

    [PM-27239] Tde registration encryption v2 (#17831)
    * tmp
    
    * Implement TDE v2 registration via SDK
    
    * Undo encstring test string change
    
    * Add feature flag
    
    * Add tests
    
    * Continue tests
    
    * Cleanup
    
    * Cleanup
    
    * run prettier
    
    * Update to apply new sdk changes
    
    * Fix build
    
    * Update package lock
    
    * Fix tests
    
    ---------
    
    Co-authored-by: Bernd Schoolmann <quexten@fedora-2.fritz.box>

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -44,6 +44,7 @@ export enum FeatureFlag {
   NoLogoutOnKdfChange = "pm-23995-no-logout-on-kdf-change",
   DataRecoveryTool = "pm-28813-data-recovery-tool",
   ConsolidatedSessionTimeoutComponent = "pm-26056-consolidated-session-timeout-component",
+  PM27279_V2RegistrationTdeJit = "pm-27279-v2-registration-tde-jit",
 
   /* Tools */
   DesktopSendUIRefresh = "desktop-send-ui-refresh",
@@ -154,6 +155,7 @@ export const DefaultFeatureFlagValue = {
   [FeatureFlag.NoLogoutOnKdfChange]: FALSE,
   [FeatureFlag.DataRecoveryTool]: FALSE,
   [FeatureFlag.ConsolidatedSessionTimeoutComponent]: FALSE,
+  [FeatureFlag.PM27279_V2RegistrationTdeJit]: FALSE,
 
   /* Platform */
   [FeatureFlag.IpcChannelFramework]: FALSE,
