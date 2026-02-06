03a60a61cbcf420441462d3d4e25440365332ae8
Daniel James Smith <2670567+djsmith85@users.noreply.github.com>
2026-02-06 22:15:12 +0100

    Revert "[PM-29149] Add ServerCommunicationConfigService (#18815)" (#18821)
    This reverts commit f1b9408e3f0101fa5e24e058b0c7880ae9c06314.

--- libs/common/src/platform/services/server-communication-config/index.ts
+++
@@ -1,3 +0,0 @@
-export { ServerCommunicationConfigRepository } from "./server-communication-config.repository";
-export { DefaultServerCommunicationConfigService } from "./default-server-communication-config.service";
-export { SERVER_COMMUNICATION_CONFIGS } from "./server-communication-config.state";
