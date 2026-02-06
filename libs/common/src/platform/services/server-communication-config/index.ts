f1b9408e3f0101fa5e24e058b0c7880ae9c06314
Daniel James Smith <2670567+djsmith85@users.noreply.github.com>
2026-02-06 21:40:03 +0100

    [PM-29149] Add ServerCommunicationConfigService (#18815)
    * Add state- and key-definitions for persisting serverCommunicationConfig(s)
    
    * Add implementation of the SDK-defined ServerCommunicationConfigRepository
    
    * Add ServerCommunicationConfigService
    
    ---------
    
    Co-authored-by: Daniel James Smith <djsmith85@users.noreply.github.com>

---
+++ libs/common/src/platform/services/server-communication-config/index.ts
@@ -0,0 +1,3 @@
+export { ServerCommunicationConfigRepository } from "./server-communication-config.repository";
+export { DefaultServerCommunicationConfigService } from "./default-server-communication-config.service";
+export { SERVER_COMMUNICATION_CONFIGS } from "./server-communication-config.state";
