e2bee489a45f79d5592d933433b24d11ab464a8c
Jared McCannon <jmccannon@bitwarden.com>
2026-03-02 10:38:54 -0600

    [PM-30886] - Remove FF for Restoring Users. (#19155)
    * removed feature flag for default user collection being created for restored users.
    
    * leaving the url as we'll need it in case we have to revert a release

--- libs/common/src/enums/feature-flag.enum.ts
+++ libs/common/src/enums/feature-flag.enum.ts
@@ -12,7 +12,6 @@ import { ServerConfig } from "../platform/abstractions/config/server-config";
 export enum FeatureFlag {
   /* Admin Console Team */
   AutoConfirm = "pm-19934-auto-confirm-organization-users",
-  DefaultUserCollectionRestore = "pm-30883-my-items-restored-users",
   BulkReinviteUI = "pm-28416-bulk-reinvite-ux-improvements",
 
   /* Auth */
@@ -110,7 +109,6 @@ const FALSE = false as boolean;
 export const DefaultFeatureFlagValue = {
   /* Admin Console Team */
   [FeatureFlag.AutoConfirm]: FALSE,
-  [FeatureFlag.DefaultUserCollectionRestore]: FALSE,
   [FeatureFlag.BulkReinviteUI]: FALSE,
 
   /* Autofill */
