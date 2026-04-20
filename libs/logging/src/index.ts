0ae06536371daeb0a8c3c3bd3a83931c71e1a870
Daniel García <dani-garcia@users.noreply.github.com>
2026-04-20 16:14:25 +0200

    [PM-33314] Add FlightRecorder at @bitwarden/logging (#20171)
    * [PM-33314] Add FlightRecorder at @bitwarden/logging
    
    * Fix tests requiring TextDecoder
    
    * Revert test.environment changes
    
    * Fix jest config

--- libs/logging/src/index.ts
+++ libs/logging/src/index.ts
@@ -1,3 +1,5 @@
 export { LogService } from "./log.service";
 export { LogLevel } from "./log-level";
 export { ConsoleLogService } from "./console-log.service";
+export { FlightRecorder } from "./flight-recorder";
+export { FlightRecorderEvent } from "@bitwarden/sdk-internal";
