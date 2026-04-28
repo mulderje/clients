ad7e67f5401259f6b9d30366c208b56d4cd522f2
Brandon Treston <btreston@bitwarden.com>
2026-04-28 15:42:47 -0400

    [PM-34409] Create new SCIM UI (#20272)
    * create new SCIM UI
    
    * fix style
    
    * update spinner

--- bitwarden_license/bit-web/src/app/admin-console/organizations/organizations-routing.module.ts
+++ bitwarden_license/bit-web/src/app/admin-console/organizations/organizations-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from "@angular/core";
+import { inject, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { map } from "rxjs";
 
 import { authGuard } from "@bitwarden/angular/auth/guards";
+import { componentRouteSwap } from "@bitwarden/angular/utils/component-route-swap";
 import {
   canAccessAccessIntelligence,
   canAccessSettingsTab,
 } from "@bitwarden/common/admin-console/abstractions/organization/organization.service.abstraction";
+import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
+import { ConfigService } from "@bitwarden/common/platform/abstractions/config/config.service";
 import { isEnterpriseOrgGuard } from "@bitwarden/web-vault/app/admin-console/organizations/guards/is-enterprise-org.guard";
 import { organizationPermissionsGuard } from "@bitwarden/web-vault/app/admin-console/organizations/guards/org-permissions.guard";
 import { OrganizationLayoutComponent } from "@bitwarden/web-vault/app/admin-console/organizations/layouts/organization-layout.component";
@@ -14,6 +18,7 @@ import { deepLinkGuard } from "@bitwarden/web-vault/app/auth/guards/deep-link/de
 import { SsoManageComponent } from "../../auth/sso/sso-manage.component";
 
 import { DomainVerificationComponent } from "./manage/domain-verification/domain-verification.component";
+import { ScimV2Component } from "./manage/scim-v2.component";
 import { ScimComponent } from "./manage/scim.component";
 
 const routes: Routes = [
@@ -42,14 +47,24 @@ const routes: Routes = [
               titleId: "singleSignOn",
             },
           },
-          {
-            path: "scim",
-            component: ScimComponent,
-            canActivate: [organizationPermissionsGuard((org) => org.canManageScim)],
-            data: {
-              titleId: "scim",
+          ...componentRouteSwap(
+            ScimComponent,
+            ScimV2Component,
+            () =>
+              inject(ConfigService)
+                .getFeatureFlag$(FeatureFlag.GenerateInviteLink)
+                .pipe(map((v) => v === true)),
+            {
+              path: "scim",
+              canActivate: [organizationPermissionsGuard((org) => org.canManageScim)],
+              data: { titleId: "scim" },
             },
-          },
+            {
+              path: "scim",
+              canActivate: [organizationPermissionsGuard((org) => org.canManageScim)],
+              data: { titleId: "scimV2" },
+            },
+          ),
           {
             path: "device-approvals",
             loadComponent: () =>
--- bitwarden_license/bit-web/src/app/admin-console/organizations/organizations.module.ts
+++ bitwarden_license/bit-web/src/app/admin-console/organizations/organizations.module.ts
@@ -7,11 +7,12 @@ import { SsoManageComponent } from "../../auth/sso/sso-manage.component";
 
 import { DomainAddEditDialogComponent } from "./manage/domain-verification/domain-add-edit-dialog/domain-add-edit-dialog.component";
 import { DomainVerificationComponent } from "./manage/domain-verification/domain-verification.component";
+import { ScimV2Component } from "./manage/scim-v2.component";
 import { ScimComponent } from "./manage/scim.component";
 import { OrganizationsRoutingModule } from "./organizations-routing.module";
 
 @NgModule({
-  imports: [SharedModule, OrganizationsRoutingModule, HeaderModule],
+  imports: [SharedModule, OrganizationsRoutingModule, HeaderModule, ScimV2Component],
   declarations: [
     SsoManageComponent,
     ScimComponent,
