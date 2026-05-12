453ae406e273467d917a64205d54dc9cc92af9ea
Bernd Schoolmann <mail@quexten.com>
2026-05-12 18:53:52 +0900

    [PM-31059] Implement state bridge support (#20394)
    * Implement state bridge support
    
    * Update package.lock
    
    * Update sdk
    
    * Prettier
    
    * Fix test mock
    
    * Cleanup
    
    * Cleanup
    
    * Fix name
    
    * Add timeout
    
    * Fix type issue
    
    * prettier
    
    * Remove unecessary try catch

---
+++ libs/common/src/key-management/v2-upgrade-token.state.ts
@@ -0,0 +1,11 @@
+import { CRYPTO_DISK, UserKeyDefinition } from "@bitwarden/common/platform/state";
+import { V2UpgradeToken } from "@bitwarden/sdk-internal";
+
+export const V2_UPGRADE_TOKEN = new UserKeyDefinition<V2UpgradeToken>(
+  CRYPTO_DISK,
+  "v2UpgradeToken",
+  {
+    deserializer: (jsonValue) => jsonValue,
+    clearOn: ["logout"],
+  },
+);
