99cdeb083d47ab4f276d16dfbcb3a6463872e0b3
Leslie Tilton <23057410+Banrion@users.noreply.github.com>
2026-05-26 08:51:25 -0500

    Fix hanging observables causing blank access intelligence page (#20822)

--- bitwarden_license/bit-common/src/dirt/access-intelligence/services/implementations/persistence/report-persistence-feature-flag.service.ts
+++ bitwarden_license/bit-common/src/dirt/access-intelligence/services/implementations/persistence/report-persistence-feature-flag.service.ts
@@ -1,4 +1,4 @@
-import { Observable, switchMap } from "rxjs";
+import { first, Observable, switchMap } from "rxjs";
 
 import { FeatureFlag } from "@bitwarden/common/enums/feature-flag.enum";
 import { EncString } from "@bitwarden/common/key-management/crypto/models/enc-string";
@@ -23,41 +23,38 @@ export class ReportPersistenceFeatureFlagService extends ReportPersistenceServic
   loadLastReport$(
     organizationId: OrganizationId,
   ): Observable<{ report: AccessReportView; hadLegacyBlobs: boolean } | null> {
-    return this.configService
-      .getFeatureFlag$(FeatureFlag.AccessIntelligenceReportFileStorage)
-      .pipe(
-        switchMap((useFileStorage) =>
-          useFileStorage
-            ? this.filePersistenceService.loadLastReport$(organizationId)
-            : this.defaultPersistenceService.loadLastReport$(organizationId),
-        ),
-      );
+    return this.configService.getFeatureFlag$(FeatureFlag.AccessIntelligenceReportFileStorage).pipe(
+      first(),
+      switchMap((useFileStorage) =>
+        useFileStorage
+          ? this.filePersistenceService.loadLastReport$(organizationId)
+          : this.defaultPersistenceService.loadLastReport$(organizationId),
+      ),
+    );
   }
 
   saveApplicationMetadata$(view: AccessReportView): Observable<void> {
-    return this.configService
-      .getFeatureFlag$(FeatureFlag.AccessIntelligenceReportFileStorage)
-      .pipe(
-        switchMap((useFileStorage) =>
-          useFileStorage
-            ? this.filePersistenceService.saveApplicationMetadata$(view)
-            : this.defaultPersistenceService.saveApplicationMetadata$(view),
-        ),
-      );
+    return this.configService.getFeatureFlag$(FeatureFlag.AccessIntelligenceReportFileStorage).pipe(
+      first(),
+      switchMap((useFileStorage) =>
+        useFileStorage
+          ? this.filePersistenceService.saveApplicationMetadata$(view)
+          : this.defaultPersistenceService.saveApplicationMetadata$(view),
+      ),
+    );
   }
 
   saveReport$(
     view: AccessReportView,
     organizationId: OrganizationId,
   ): Observable<{ id: OrganizationReportId; contentEncryptionKey: EncString }> {
-    return this.configService
-      .getFeatureFlag$(FeatureFlag.AccessIntelligenceReportFileStorage)
-      .pipe(
-        switchMap((useFileStorage) =>
-          useFileStorage
-            ? this.filePersistenceService.saveReport$(view, organizationId)
-            : this.defaultPersistenceService.saveReport$(view, organizationId),
-        ),
-      );
+    return this.configService.getFeatureFlag$(FeatureFlag.AccessIntelligenceReportFileStorage).pipe(
+      first(),
+      switchMap((useFileStorage) =>
+        useFileStorage
+          ? this.filePersistenceService.saveReport$(view, organizationId)
+          : this.defaultPersistenceService.saveReport$(view, organizationId),
+      ),
+    );
   }
 }
