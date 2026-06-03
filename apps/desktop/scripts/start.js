32507c1e0b30f4b6c1b5d4150451b132707a71e0
Bernd Schoolmann <mail@quexten.com>
2026-06-03 19:33:05 +0900

    [BEEEP] [PM-38497] Dev-ex: Add attach VSCode debugger to desktop, web configs (#21016)
    * Add attach debugger configs for vscode
    
    * Fix desktop not starting

--- apps/desktop/scripts/start.js
+++ apps/desktop/scripts/start.js
@@ -25,7 +25,7 @@ concurrently(
     },
     {
       name: "Elec",
-      command: `npx wait-on ./build/main.js && npx electron --no-sandbox --inspect=5858 ${args.join(
+      command: `npx wait-on ./build/main.js ./build/index.html ./build/app/main.js && npx electron --no-sandbox --inspect=5858 --remote-debugging-port=9222 ${args.join(
         " ",
       )} ./build --watch`,
       prefixColor: "green",
