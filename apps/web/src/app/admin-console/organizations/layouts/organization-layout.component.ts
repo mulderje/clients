38c6d9aea0b64c122de0bfd718190d7d30050770
Brandon Treston <btreston@bitwarden.com>
2026-06-09 13:53:42 -0400

    revert onPush change detection in org layout component (#21147)

--- apps/web/src/app/admin-console/organizations/layouts/organization-layout.component.ts
+++ apps/web/src/app/admin-console/organizations/layouts/organization-layout.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { ActivatedRoute, RouterModule } from "@angular/router";
 import { combineLatest, filter, map, Observable, switchMap, withLatestFrom } from "rxjs";
 
@@ -36,10 +36,11 @@ import { FreeFamiliesPolicyService } from "../../../billing/services/free-famili
 import { OrgSwitcherComponent } from "../../../layouts/org-switcher/org-switcher.component";
 import { WebLayoutModule } from "../../../layouts/web-layout.module";
 
+// FIXME(https://bitwarden.atlassian.net/browse/CL-764): Migrate to OnPush
+// eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
 @Component({
   selector: "app-organization-layout",
   templateUrl: "organization-layout.component.html",
-  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
     CommonModule,
     RouterModule,
