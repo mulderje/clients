a1ce4c8ab86796fea21fc4d1443f3c0d20778eb4
Bernd Schoolmann <mail@quexten.com>
2026-06-10 13:08:23 +0000

    Initialize feature flag overrides state via migration (#21166)

--- libs/state/src/state-migrations/migrate.ts
+++ libs/state/src/state-migrations/migrate.ts
@@ -75,12 +75,13 @@ import { RemoveUserEncryptedPrivateKey } from "./migrations/75-remove-user-encry
 import { MigratePopupWidthOptions } from "./migrations/76-migrate-popup-width-options";
 import { ClearClipboardDelayToStringMigrator } from "./migrations/77-clear-clipboard-delay-to-string";
 import { MigrateSsoRequiredCache } from "./migrations/78-migrate-sso-required-cache";
+import { InitializeFeatureFlagOverridesMigrator } from "./migrations/79-initialize-feature-flag-overrides";
 import { MoveStateVersionMigrator } from "./migrations/8-move-state-version";
 import { MoveBrowserSettingsToGlobal } from "./migrations/9-move-browser-settings-to-global";
 import { MinVersionMigrator } from "./migrations/min-version";
 
 export const MIN_VERSION = 3;
-export const CURRENT_VERSION = 78;
+export const CURRENT_VERSION = 79;
 export type MinVersion = typeof MIN_VERSION;
 
 export function createMigrationBuilder() {
@@ -160,7 +161,8 @@ export function createMigrationBuilder() {
     .with(RemoveUserEncryptedPrivateKey, 74, 75)
     .with(MigratePopupWidthOptions, 75, 76)
     .with(ClearClipboardDelayToStringMigrator, 76, 77)
-    .with(MigrateSsoRequiredCache, 77, CURRENT_VERSION);
+    .with(MigrateSsoRequiredCache, 77, 78)
+    .with(InitializeFeatureFlagOverridesMigrator, 78, CURRENT_VERSION);
 }
 
 export async function currentVersion(
