981f1ac90db8c6c87a98fcf537dba45ebb27424b
Isaiah Inuwa <iinuwa@bitwarden.com>
2026-07-01 15:04:50 -0500

    Add credential request processing (#21618)

--- apps/desktop/desktop_native/windows_plugin_authenticator/src/main.rs
+++ apps/desktop/desktop_native/windows_plugin_authenticator/src/main.rs
@@ -1,1 +1,9 @@
+#![cfg_attr(target_os = "windows", expect(unused))]
+#[cfg(target_os = "windows")]
+mod assert;
+#[cfg(target_os = "windows")]
+mod make_credential;
+#[cfg(target_os = "windows")]
+mod util;
+
 fn main() {}
