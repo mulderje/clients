dc65f3516c89571fd302e888f9cb2ea03c677bc0
Jared <TheWolfBadger@gmail.com>
2026-09-10 11:38:09 -0400

    Fix terminology changes for VFO 1 (#23080)

--- bitwarden_license/bit-web/src/app/billing/policies/free-families-sponsorship.component.ts
+++ bitwarden_license/bit-web/src/app/billing/policies/free-families-sponsorship.component.ts
@@ -10,7 +10,7 @@ export class FreeFamiliesSponsorshipPolicy extends BasePolicyEditDefinition {
   nameVfo1 = "freeFamiliesSponsorshipPolicyTitleV2Vfo1";
   drawerNameVfo1 = "freeFamiliesSponsorshipPolicyTitleV2Vfo1";
   description = "freeFamiliesSponsorshipPolicyDescV2";
-  descriptionVfo1 = "freeFamiliesSponsorshipDescVfo1";
+  descriptionVfo1 = "freeFamiliesSponsorshipDescV2Vfo1";
   type = PolicyType.FreeFamiliesSponsorship;
   category = PolicyCategory.VaultManagement;
   priority = 60;
